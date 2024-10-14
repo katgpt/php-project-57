67e1d9b20f0a5708520e5b8dd0b17a7a88d513c5
siteservicehelp <siteservicehelp@gmail.com>
2024-10-14 19:56:01 +0600

    step7

--- database/migrations/2024_10_14_123747_create_sessions_table.php
+++ database/migrations/2024_10_14_123747_create_sessions_table.php
@@ -11,12 +11,12 @@ return new class extends Migration {
     public function up(): void {
         Schema::create('sessions', function (Blueprint $table) {
             $table->string('id')->primary(); // Первичный ключ
-            $table->timestamps();
             $table->foreignId('user_id')->nullable()->index();
             $table->string('ip_address', 45)->nullable();
             $table->text('user_agent')->nullable();
             $table->text('payload');
             $table->integer('last_activity')->index();
+            $table->timestamps();
         });
     }
 
