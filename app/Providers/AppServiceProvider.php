f91963998083293d318aa034d51fa798be952f09
katgpt <katgpt2024@gmail.com>
2024-10-18 02:57:48 +0600

    step 6 finish

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,9 +25,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (env('APP_ENV') === 'production') {
+        /*if (env('APP_ENV') === 'production') {
             URL::forceScheme('https');
-        }
+        }*/
         Paginator::useTailwind();
     }
 }
