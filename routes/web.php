2218b56393cbff44869d8c8bfcf0ee5b065361c3
siteservicehelp <36890464+siteservicehelp@users.noreply.github.com>
2024-10-25 16:23:13 +0600

    bugs test

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ Route::get('/', function () {
 })->name('welcome');
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return redirect('/');  // Временно перенаправляем на корневой путь
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 // Маршруты для профиля
