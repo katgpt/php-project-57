bea9b5997a08558012d49c2177ba55607e186208
siteservicehelp <36890464+siteservicehelp@users.noreply.github.com>
2024-10-26 17:51:39 +0600

    bugs test2

--- resources/views/labels/index.blade.php
+++ resources/views/labels/index.blade.php
@@ -1,52 +1,62 @@
-@extends('layouts.app')
-@section('content')
-
-<div class="grid col-span-full">
-    <h1 class="max-w-2xl mb-4 text-4xl leading-none tracking-tight md:text-5xl xl:text-6xl">
-        {{ __('layout.labels_header') }} </h1>
-    @auth()
-    <div>
-        @csrf
-        <a href="{{ route('labels.create') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            {{ __('layout.labels_create') }}           </a>
-    </div>
-    @endauth
-    <table class="mt-4">
-        <thead class="border-b-2 border-solid border-black text-left" style="text-align: left">
-        <tr>
-            <th>{{ __('layout.table_id') }}</th>
-            <th>{{ __('layout.table_name') }}</th>
-            <th>{{ __('layout.table_description') }}</th>
-            <th>{{ __('layout.table_date_of_creation') }}</th>
-            @auth()
-            <th>{{ __('layout.table_actions') }}</th>
-            @endauth
-        </tr>
-        </thead>
-        <tbody>
-        @foreach($labels as $label)
-        <tr class="border-b border-dashed text-left">
-            <td>{{ $label->id }}</td>
-            <td>{{ $label->name }}</td>
-            <td>{{ $label->description }}</td>
-            <td>{{ $label->created_at->format('d.m.Y') }}</td>
-            @auth()
-            <td>
-                <form action="{{ route('labels.destroy', $label) }}" method="POST" onsubmit="return confirm('{{ __("layout.table_delete_question") }}');">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit" class="text-red-600 hover:text-red-900">{{ __('layout.table_delete') }}</button>
-                </form>
-                <a class="text-blue-600 hover:text-blue-900"
-                   href="{{ route('labels.edit', $label) }}"
-                >
-                    {{ __('layout.table_edit') }}
-                </a>
-            </td>
-            @endauth
-        </tr>
-            @endforeach
-        </tbody>
-    </table>
-</div>
-@endsection
+<x-app-layout>
+    <x-slot name="header">
+        @include('layouts.navigation')
+    </x-slot>
+    <section class="bg-white">
+        <div class="max-w-screen-xl px-4 pt-28 pb-8 mx-auto lg:pb-16">
+            @include('flash::message')
+            <div class="grid col-span-full">
+                <h1 class="mb-5">{{ __('Labels') }}</h1>
+                <div class="w-full flex items-center">
+                    @can('create', \App\Models\Label::class)
+                    <div class="ml-auto">
+                        <x-primary-hyperlink-button :href="route('labels.create')">
+                            {{ __('label.create-label') }}
+                        </x-primary-hyperlink-button>
+                    </div>
+                    @endcan
+                </div>
+                <table class="mt-4">
+                    <thead class="border-b-2 border-solid border-black text-left">
+                    <tr>
+                        <th>{{ __('ID') }}</th>
+                        <th>{{ __('Name') }}</th>
+                        <th>{{ __('Description') }}</th>
+                        <th>{{ __('Date of creation') }}</th>
+                        @canany(['update', 'delete'], new \App\Models\Label())
+                            <th>{{ __('Actions') }}</th>
+                        @endcanany
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach ($labels as $label)
+                        <tr class="border-b border-dashed text-left">
+                            <td>{{ $label->id }}</td>
+                            <td>{{ $label->name }}</td>
+                            <td>{{ $label->description }}</td>
+                            <td>{{ $label->created_at->format('d.m.Y') }}</td>
+                            @canany(['update', 'delete'], $label)
+                                <td>
+                                    @can('delete', $label)
+                                        <a class="text-red-600 hover:text-red-900"
+                                           href="{{ route('labels.destroy', $label) }}"
+                                           data-confirm="{{ __('Are you sure?') }}"
+                                           data-method="delete"
+                                           rel="nofollow">
+                                            {{ __('Delete') }}</a>
+                                    @endcan
+                                    @can('update', $label)
+                                        <a class="text-blue-600 hover:text-blue-900"
+                                           href="{{ route('labels.edit', $label) }}">
+                                            {{ __('Edit') }}</a>
+                                    @endcan
+                                </td>
+                            @endcanany
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </section>
+</x-app-layout>
