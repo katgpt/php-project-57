fb7e59fa14c8d98bc81f0a8496aec3bae51ad2f4
siteservicehelp <36890464+siteservicehelp@users.noreply.github.com>
2024-10-26 15:40:13 +0600

    bugs test2

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
 
-Route::get('/', function () {
+Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
