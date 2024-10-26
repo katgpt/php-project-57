c97506165c3ea1383ba0b38c4d0f3a4224d4a00c
siteservicehelp <36890464+siteservicehelp@users.noreply.github.com>
2024-10-26 18:21:48 +0600

    bugs test3

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,23 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
-    ],
+  plugins: [
+    laravel({
+      input: [
+        'resources/css/app.css',
+        'resources/js/app.js',
+      ],
+      refresh: true,
+    }),
+  ],
+  resolve: {
+    alias: {
+      'rails-ujs': 'node_modules/@rails/ujs/lib/assets/compiled/rails-ujs.js',
+    }
+  },
+  build: {
+    rollupOptions: {
+      external: ['rails-ujs']
+    }
+  }
 });
