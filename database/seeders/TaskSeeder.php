9cd1c0d000cd1cc5512f8eec6473f6ec9d62fa60
siteservicehelp <36890464+siteservicehelp@users.noreply.github.com>
2024-10-28 16:20:42 +0600

    bugs test4

--- database/seeders/TaskSeeder.php
+++ database/seeders/TaskSeeder.php
@@ -20,7 +20,7 @@ class TaskSeeder extends Seeder
             ['name' => 'выполняется'],
             ['name' => 'в архиве'],
         ];
-        
+                
         foreach ($statusNames as $statusName) {
             TaskStatus::create($statusName);
         }
